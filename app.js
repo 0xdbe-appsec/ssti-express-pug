7dc19c18039ea7fc31d26e680298bf87cc6e3580
0xdbe <firstname@lastname.net>
2020-09-15 21:18:53 +0200

    fix using pug interpolation

--- app.js
+++ app.js
@@ -15,13 +15,17 @@ app.get('/', (req, res) => {
     if (err) throw err;
               
     // Customize greeting message
+    /*
     if(typeof req.query.name != 'undefined'){
       console.log(req.query.name);
       template = template.replace(/world/g, req.query.name);
     }
+    */
+    
+    typeof req.query.name != 'undefined' ? name = req.query.name : name = 'world';
    
     // Send HTML
-    let html = pug.render(template)
+    let html = pug.render(template, { name: name })
     res.set('Content-Type', 'text/html');
     res.send(html);
   });
